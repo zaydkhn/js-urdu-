19d89066c17b0e02a74d0a25654becc5ef04258a
zaydkhn <ayatfatima12max@gmail.com>
2024-05-19 06:44:30 +0000

    lecture4

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 1445534
+let accountEmail = "zaydi@gmail.accountEmail"
+var accountPassword = "12345"
+accountCity = "swat"
+let accountState
+
+// acccountId = 2 // not allowed
+
+accountEmail = "hd@hv.com"
+accountPassword = "4324"
+accountCity = "islamabad"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+dont use var
+because of issue in block scope and functional scope
+*/
