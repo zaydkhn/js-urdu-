ccc34fdaaa4fb40cb706be749e212e75f7e93b38
zaydkhn <ayatfatima12max@gmail.com>
2024-05-20 06:00:03 +0000

    daraTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict" //use all JS code as newer version
+
+// alert( 3 + 3) we are using nodejs, not browser
+
+//code readability should be high
+
+let name = "zayd" // string
+let age = 20 // number
+let isLoggedIn = false // bolean
+
+
+// number => 2 to the power 53
+// bigint if number is very big
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
