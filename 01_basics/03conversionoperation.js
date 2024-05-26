7087f7f98c2edf146ab98dbcb581878e6993d7a4
zaydkhn <ayatfatima12max@gmail.com>
2024-05-26 06:18:29 +0000

    conversionOfDataTypes

---
+++ 01_basics/03conversionoperation.js
@@ -0,0 +1,38 @@
+let score = "zayd"
+
+//const {score}req.body
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+
+
+//NaN(not a number)
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc"  => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//"" => false
+//"zays" => true
+
+
+let someNumber = 33
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+
+ console.log(typeof stringNumber);
+
+ 
