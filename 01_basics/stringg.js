662fd78ca96a878d4482e1b34ebcc35918ecd886
zaydkhn <ayatfatima12max@gmail.com>
2024-06-01 09:56:51 +0000

    stringghehe

---
+++ 01_basics/stringg.js
@@ -0,0 +1,44 @@
+//const name = "zayd"
+//const repoCount = 23
+
+//console.log(name + repoCount + "value");
+
+
+//console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String('zayd-zyd')
+
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+//console.log(gameName.charAt(3));
+//console.log(gameName.indexOf('a'));
+
+
+//const newString = gameName.substring(0, 4)
+
+//console.log(newString);
+
+//const anotherString = gameName.slice(0, 4)
+//console.log(anotherString);
+
+
+
+
+const newString1 = "    zayd    "
+
+//console.log(newString1);
+//console.log(newString1.trim());
+
+
+const url = "https://zayd.com/zayd%20farman"
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('zayd'));
+
+console.log(gameName.split('-'));
+
+
