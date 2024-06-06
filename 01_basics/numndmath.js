3d30a8683f529cb90a311e3058c2ab16dd421b8d
zaydkhn <ayatfatima12max@gmail.com>
2024-06-06 06:17:56 +0000

    numndmath

---
+++ 01_basics/numndmath.js
@@ -0,0 +1,42 @@
+//const score = 100
+//const balance - new Number(200)
+
+//console.log(balance.toLocaleString());
+//console.log(balance.toString());
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+//console.log(balance.toPrecision(3));
+
+
+
+//*******************MATH***************
+
+
+//console.log(Math);
+//console.log(Math.abs(-4));
+//console.log(Math.round(4.3));
+//console.log(Math.ceil(4.2));
+//console.log(Math.floor(4.2));
+//console.log(Math.min(4, 5, 6, 2));
+//console.log(Math.max(4, 5, 6, 2));
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.random()*10 + 1);
+// console.log((Math.random()*10) +1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+// console.log(Math.random() * (max - min + 1) + min)
+console.log(Math.floor(Math.random() * (max - min + 1)))
+
+
+
+
+
+
+
+
+
