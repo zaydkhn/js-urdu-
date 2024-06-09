ac2bd93eade37c55e2d9c090b678fec56eba2d07
zaydkhn <ayatfatima12max@gmail.com>
2024-06-09 06:49:06 +0000

    halfdai

---
+++ 01_basics/datesin.js
@@ -0,0 +1,38 @@
+
+//DATES
+//date is an object
+
+let myDate = new Date()
+
+// console.log(myDate);//2024-06-09T06:27:12.852Z
+
+// console.log(myDate.toString()); //sun june 09 2024 06;28;44 GMT+0000 (coordinated universal time)
+
+// console.log(myDate.toDateString()); //Sun Jun 09 2024
+
+// console.log(myDate.toISOString()); //2024-06-09T06:33:10.305Z
+
+// console.log(myDate.toJSON()); //2024-06-09T06:34:13.246Z
+
+// console.log(myDate.toLocaleDateString()); //  6/9/2024
+
+//console.log(myDate.toLocaleTimeString()); // 6:37:38 AM
+
+let heheDate = new Date(2024, 0, 23)
+console.log(heheDate.toDateString());
+
+let eedate = new Date("2023-01-30")
+console.log(eedate.toLocaleString());
+
+//////////////////TIMESTAMP/////////////////
+
+
+
+
+
+
+
+
+
+
+
