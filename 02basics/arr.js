b16f10b77af96188b0dba7182c435c5d68dfbba0
zaydkhn <ayatfatima12max@gmail.com>
2024-06-27 12:31:19 +0000

    arrayndsht

---
+++ 02basics/arr.js
@@ -0,0 +1,38 @@
+// array 
+
+const myArr = [0, 1, 2, 3, 4, 5]
+
+const myHeros = ["zayd", "me"]
+
+
+const myArr2 = new Array(1, 2, 3, 4)
+
+
+console.log(myArr[0]);
+
+// array methods
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
