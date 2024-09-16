692fb74d7fe1245d6b4f976a294a8b03ad226981
zaydkhn <ayatfatima12max@gmail.com>
2024-09-16 06:43:13 +0000

    strhe shm

---
+++ ten/inhr.js
@@ -0,0 +1,36 @@
+class User {
+    constructor(username){
+        this.username = username 
+    }
+
+    logMe(){
+        console.log(`USERNAME is ${this.username}`);
+        
+    }
+}
+
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username)
+        this.email = email
+        this.password = password
+    }
+
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);
+        
+    }
+}
+
+const chai = new Teacher('chai', 'chai@tchr.com', '123')
+
+chai.logMe()
+
+const tea = new User('tea')
+
+//addCourse()
+
+tea.logMe()
+
+
+console.log(chai instanceof User)
